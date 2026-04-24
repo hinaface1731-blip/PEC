82e48491e09d13bbd20cf091448c5319804c7a12
hinaface1731-blip <hinaface1731@gmail.com>
2026-04-24 10:27:10 +0700

    Updates

---
+++ _next/static/OeSzb71i276Tvk_vvEb6f/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PEC//_next/:path+",
+        "destination": "/PEC/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
