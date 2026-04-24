89e845fbaaef2e69ebea00088a202b4ab92c6591
hinaface1731-blip <hinaface1731@gmail.com>
2026-04-24 10:37:06 +0700

    Updates

--- _next/static/CJSpdCKwSBSc-6VrsRsQ3/_buildManifest.js
+++ _next/static/CJSpdCKwSBSc-6VrsRsQ3/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/PEC//_next/:path+",
-        "destination": "/PEC/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
